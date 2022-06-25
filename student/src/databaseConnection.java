9b4eac240b2afb38561d51312cdec7ca173602ba
ananyo-victor <ananyomaitroan@gmail.com>
2022-06-25 23:35:22 +0530

    new

---
+++ student/src/databaseConnection.java
@@ -0,0 +1,37 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ranaj
+ */
+public class databaseConnection {
+    
+    // C:\Program Files\Java\jdk1.8.0_251\jre\lib\ext
+    final static String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    final static String DB_URL = "jdbc:mysql://localhost:3306/student";
+    
+    final static String USER = "root";
+    final static String PASS = "";
+    
+    public static Connection connection()
+    {
+        try{
+            Class.forName(JDBC_DRIVER);
+            Connection conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            return conn;
+        }catch(ClassNotFoundException | SQLException e){
+            JOptionPane.showMessageDialog(null,"Database is not connected");
+        }
+        return null;
+}
+}
